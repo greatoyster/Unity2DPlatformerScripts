9ad46f02c593ba93d2f5cccb3d5f2cb206ab477e
greatoyster <40665014+greatoyster@users.noreply.github.com>
2019-05-23 22:15:36 +0800

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 400f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove= Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump")) 
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch")) 
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+
+
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove *runSpeed* Time.deltaTime, false, jump);
+        jump = false;
+
+    }
+}
